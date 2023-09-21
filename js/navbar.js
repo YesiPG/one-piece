ca301b64e27a9fd5184f281f951fd0b678ea6954
Yesica Perlaza <perlazayesik@gmail.com>
2023-09-20 19:24:02 -0500

    empezando hero

---
+++ js/navbar.js
@@ -0,0 +1,20 @@
+(function () {
+  "use strict";
+
+  const d = document;
+  const nav = d.querySelector(".contH-nav");
+  const close = d.querySelector(".contH-nav-close");
+  const menu = d.querySelector(".contH-menu");
+
+  menu.addEventListener("click", (e) => {
+    nav.classList.add("contH-nav-show");
+    close.classList.add("contH-nav-close-show");
+    menu.classList.add("contH-menu-show");
+  });
+
+  close.addEventListener("click",(e)=>{
+    nav.classList.remove("contH-nav-show");
+    close.classList.remove("contH-nav-close-show");
+    menu.classList.remove("contH-menu-show");
+  })
+})();
