18bb9e8db2959c5dfa5d6f83071db397e720ca28
Yesica Perlaza <perlazayesik@gmail.com>
2023-09-28 23:50:04 -0500

    arreglando detalles

--- js/navbar.js
+++ js/navbar.js
@@ -5,6 +5,9 @@
   const nav = d.querySelector(".contH-nav");
   const close = d.querySelector(".contH-nav-close");
   const menu = d.querySelector(".contH-menu");
+  const nav2 = d.querySelector(".contH-tripulacion");
+  const btnT = d.querySelector(".tripulacion");
+  const close2 = d.querySelector(".contH-tripulacion-close");
 
   menu.addEventListener("click", (e) => {
     nav.classList.add("contH-nav-show");
@@ -16,5 +19,13 @@
     nav.classList.remove("contH-nav-show");
     close.classList.remove("contH-nav-close-show");
     menu.classList.remove("contH-menu-show");
+  })
+
+  btnT.addEventListener("click", (e)=>{
+    nav2.classList.add("contH-tripulacion-show")
+  });
+
+  close2.addEventListener("click",(e)=>{
+    nav2.classList.remove("contH-tripulacion-show");
   })
 })();
